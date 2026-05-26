6ebdff70f262e134a421bcd7ac496f881e0bac43
Emam546 <emam54637@gmail.com>
2026-05-26 11:27:25 +0300

    add logger an update the updating method

--- app/main/updater/index.ts
+++ app/main/updater/index.ts
@@ -1,32 +1,42 @@
-import { app } from "electron";
+import { app, Notification } from "electron";
 // import { autoUpdater } from "electron-updater";
 import { createUpdateWindow } from "@app/main/lib/update";
 import AppUpdater from "./AppUpdater";
 import PackageJson from "../../../package.json";
 import { isDev, isProd } from "../utils";
 import { MainWindow } from "../lib/main/window";
-console.log("Version", PackageJson.version);
+import { logger } from "../helpers/logger";
+logger.info(`Version ${PackageJson.version}`);
 const autoUpdater = new AppUpdater({
   owner: PackageJson.publish.owner,
   releaseType: PackageJson.publish.releaseType as any,
   repo: PackageJson.publish.repo,
 });
 app.whenReady().then(async () => {
-  autoUpdater.on("error", (e) => console.error(e));
-  console.log("checking for update");
+  autoUpdater.on("error", (e) => logger.err(e));
+  logger.info("checking for update");
   if (isProd) autoUpdater.checkForUpdates();
 });
 autoUpdater.once("update-available", (update) => {
-  console.log("update available", update.tag_name);
-  console.log("Download the update");
-  autoUpdater.downloadUpdate(update).then((asset) => {
-    autoUpdater.once("updater-downloaded", (savedFilePath) => {
-      console.log("update finished");
-      autoUpdater.quitAndInstall(savedFilePath);
-    });
+  logger.info(`update available ${update.tag_name}`);
+  logger.info("Downloading the update");
+  const notification = new Notification({
+    title: "Update Available",
+    body: `Version ${update.tag_name} is available. Click to download.`,
+  });
+
+  notification.show();
+  notification.on("click", async () => {
+    logger.info("User accepted update");
+    await autoUpdater.downloadUpdate(update);
+  });
+  autoUpdater.once("updater-downloaded", (savedFilePath) => {
+    logger.info("update finished");
+    autoUpdater.quitAndInstall(savedFilePath);
   });
   autoUpdater.once("metadata", async (metadata) => {
-    console.log("start downloading");
+    logger.info("start downloading");
+    MainWindow.Window?.hide();
     await createUpdateWindow({
       preloadData: {
         curSize: 0,
@@ -35,9 +45,8 @@ autoUpdater.once("update-available", (update) => {
       autoUpdater: autoUpdater,
     });
   });
-  MainWindow.Window?.hide();
 });
 autoUpdater.once("update-not-available", () => {
-  console.log("update not available");
+  logger.info("update not available");
 });
 export default autoUpdater;
